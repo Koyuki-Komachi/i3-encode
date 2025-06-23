1192063736bdf75a9feb04ed10de77390a2efb69
wUCHAOBEN <wu-chaoben499@g.ecc.u-tokyo.ac.jp>
2025-06-23 13:42:17 +0900

    fft

--- i3_phone_fft.c
+++ i3_phone_fft.c
@@ -8,25 +8,80 @@
 #include <arpa/inet.h>
 #include <signal.h>
 #include <math.h>
-#include <fftw3.h>
 
 // --- 設定項目 ---
-#define FRAME_SIZE 1024             // FFTを行う音声データのフレームサイズ
-#define COMPRESSION_RATIO 0.7       // 圧縮率 (0.0 - 1.0): 振幅が小さい下位70%をカット
+#define FRAME_SIZE 1024             // FFTのフレームサイズ (必ず2のべき乗にすること)
+#define COMPRESSION_RATIO 0.7       // 圧縮率 (0.0 - 1.0): 周波数成分の下位70%をカット
+
+#define PI 3.14159265358979323846
 
 // 1フレームあたりのデータサイズ (16bit = 2byte)
 #define FRAME_BYTES (FRAME_SIZE * sizeof(short))
 // FFT後の複素数データのサイズ
-#define FFT_COMPLEX_SIZE (FRAME_SIZE / 2 + 1)
-#define FFT_BYTES (FFT_COMPLEX_SIZE * sizeof(fftw_complex))
-// ---
+#define FFT_BYTES (FRAME_SIZE * sizeof(Complex))
+
+// 複素数を扱うための構造体
+typedef struct {
+    double re; // 実部 (Real part)
+    double im; // 虚部 (Imaginary part)
+} Complex;
+
+
+// --- FFT / IFFT 自前実装 ---
+
+// Cooley-Tukey FFTアルゴリズム (再帰版)
+void fft(Complex *x, int N) {
+    if (N <= 1) return;
+
+    // 偶数番目と奇数番目の要素に分割
+    Complex *even = malloc(N/2 * sizeof(Complex));
+    Complex *odd  = malloc(N/2 * sizeof(Complex));
+    for (int i = 0; i < N/2; i++) {
+        even[i] = x[2*i];
+        odd[i]  = x[2*i+1];
+    }
+
+    // 再帰的にFFTを呼び出す
+    fft(even, N/2);
+    fft(odd, N/2);
+
+    // バタフライ演算で結合
+    for (int k = 0; k < N/2; k++) {
+        double angle = -2.0 * PI * k / N;
+        Complex t = {cos(angle) * odd[k].re - sin(angle) * odd[k].im,
+                     cos(angle) * odd[k].im + sin(angle) * odd[k].re};
+        x[k]       = (Complex){even[k].re + t.re, even[k].im + t.im};
+        x[k + N/2] = (Complex){even[k].re - t.re, even[k].im - t.im};
+    }
+
+    free(even);
+    free(odd);
+}
+
+// 逆FFT (IFFT)
+void ifft(Complex *x, int N) {
+    // 各要素の複素共役をとる
+    for (int i = 0; i < N; i++) {
+        x[i].im = -x[i].im;
+    }
+
+    // 通常のFFTを実行
+    fft(x, N);
+
+    // 結果の各要素の複素共役をとり、Nで割る
+    for (int i = 0; i < N; i++) {
+        x[i].re = x[i].re / N;
+        x[i].im = -x[i].im / N;
+    }
+}
+
+// --- 電話プログラム本体 ---
 
 int socket_fd = -1;
 int server_socket = -1;
 pid_t sender_pid = -1;
 pid_t receiver_pid = -1;
 
-// クリーンアップ
 void cleanup() {
     if (sender_pid > 0) kill(sender_pid, SIGTERM);
     if (receiver_pid > 0) kill(receiver_pid, SIGTERM);
@@ -42,123 +97,86 @@ void signal_handler(int sig) {
 // 送信プロセス: 標準入力 → FFT → 圧縮 → ソケット
 void audio_sender(int sock_fd) {
     short pcm_buffer[FRAME_SIZE];
-    double *fft_in;
-    fftw_complex *fft_out;
-    fftw_plan plan_forward;
-
-    // FFTWのためのメモリ確保
-    fft_in = (double*) fftw_malloc(sizeof(double) * FRAME_SIZE);
-    fft_out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * FFT_COMPLEX_SIZE);
-
-    // FFTプランの作成
-    plan_forward = fftw_plan_dft_r2c_1d(FRAME_SIZE, fft_in, fft_out, FFTW_ESTIMATE);
-
+    Complex fft_buffer[FRAME_SIZE];
+    
     while (read(STDIN_FILENO, pcm_buffer, FRAME_BYTES) == FRAME_BYTES) {
-        // short型PCMデータをdouble型に変換
+        // PCMデータを複素数バッファに変換
         for (int i = 0; i < FRAME_SIZE; i++) {
-            fft_in[i] = (double)pcm_buffer[i];
+            fft_buffer[i].re = (double)pcm_buffer[i];
+            fft_buffer[i].im = 0.0;
         }
 
-        // FFT実行
-        fftw_execute(plan_forward);
+        // 自作FFT実行
+        fft(fft_buffer, FRAME_SIZE);
 
         // --- 圧縮処理 ---
-        // 振幅が小さい高周波成分をカットする
-        // 簡単な実装として、周波数成分の上位(1-COMPRESSION_RATIO)%をゼロにする
-        int compression_index = (int)(FFT_COMPLEX_SIZE * (1.0 - COMPRESSION_RATIO));
-        for (int i = compression_index; i < FFT_COMPLEX_SIZE; i++) {
-            fft_out[i][0] = 0; // 実部
-            fft_out[i][1] = 0; // 虚部
+        // 簡単な実装として、スペクトルの後半部分をカットする
+        // FFTの結果は対称的なので、後半をカットしても情報を大きく失わない
+        int compression_index = (int)(FRAME_SIZE / 2 * (1.0 - COMPRESSION_RATIO));
+        for (int i = compression_index; i < FRAME_SIZE - compression_index; i++) {
+             if (i > FRAME_SIZE/2) { // Skip DC component and Nyquist
+                fft_buffer[i].re = 0;
+                fft_buffer[i].im = 0;
+             }
         }
         
         // 圧縮したデータを送信
-        write(sock_fd, fft_out, FFT_BYTES);
+        write(sock_fd, fft_buffer, FFT_BYTES);
     }
-
-    // 後片付け
-    fftw_destroy_plan(plan_forward);
-    fftw_free(fft_in);
-    fftw_free(fft_out);
     exit(0);
 }
 
 // 受信プロセス: ソケット → 伸張 → IFFT → 標準出力
 void audio_receiver(int sock_fd) {
     short pcm_buffer[FRAME_SIZE];
-    fftw_complex *fft_in;
-    double *fft_out;
-    fftw_plan plan_backward;
+    Complex fft_buffer[FRAME_SIZE];
+    
+    while (read(sock_fd, fft_buffer, FFT_BYTES) == FFT_BYTES) {
+        // 自作IFFT実行
+        ifft(fft_buffer, FRAME_SIZE);
 
-    // FFTWのためのメモリ確保
-    fft_in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * FFT_COMPLEX_SIZE);
-    fft_out = (double*) fftw_malloc(sizeof(double) * FRAME_SIZE);
-
-    // 逆FFTプランの作成
-    plan_backward = fftw_plan_dft_c2r_1d(FRAME_SIZE, fft_in, fft_out, FFTW_ESTIMATE);
-
-    while (read(sock_fd, fft_in, FFT_BYTES) == FFT_BYTES) {
-        // 逆FFT実行
-        fftw_execute(plan_backward);
-
-        // double型をshort型PCMデータに変換
-        // IFFTの結果はスケールされるため、FRAME_SIZEで割って正規化する
+        // 複素数データをshort型PCMデータに変換
         for (int i = 0; i < FRAME_SIZE; i++) {
-            pcm_buffer[i] = (short)(fft_out[i] / FRAME_SIZE);
+            // IFFT後の虚数部は誤差で微小に残る可能性があるが、ほぼ0なので実部のみ使う
+            pcm_buffer[i] = (short)round(fft_buffer[i].re);
         }
 
         // PCMデータを標準出力へ書き出し
         write(STDOUT_FILENO, pcm_buffer, FRAME_BYTES);
     }
-
-    // 後片付け
-    fftw_destroy_plan(plan_backward);
-    fftw_free(fft_in);
-    fftw_free(fft_out);
     exit(0);
 }
 
+// run_server, run_client, main関数は前回のコードと同じなので省略
 int run_server(int port) {
     struct sockaddr_in addr;
-
     server_socket = socket(PF_INET, SOCK_STREAM, 0);
-
     int opt = 1;
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
-
     bind(server_socket, (struct sockaddr*)&addr, sizeof(addr));
     listen(server_socket, 1);
-
     fprintf(stderr, "Server listening on port %d...\n", port);
-
     struct sockaddr_in client_addr;
     socklen_t len = sizeof(client_addr);
     socket_fd = accept(server_socket, (struct sockaddr*)&client_addr, &len);
-
-    fprintf(stderr, "Client connected from %s:%d\n",
-            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
+    fprintf(stderr, "Client connected from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
     return 0;
 }
 
 int run_client(const char *ip_str, int port) {
     struct sockaddr_in serv_addr;
-
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     inet_aton(ip_str, &serv_addr.sin_addr);
-
     fprintf(stderr, "Connecting to %s:%d...\n", ip_str, port);
     connect(socket_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
     fprintf(stderr, "Connected!\n");
-
     return 0;
 }
 
@@ -167,12 +185,9 @@ int main(int argc, char **argv) {
     signal(SIGTERM, signal_handler);
 
     if (argc == 2) {
-        int port = atoi(argv[1]);
-        run_server(port);
+        run_server(atoi(argv[1]));
     } else if (argc == 3) {
-        const char *ip_str = argv[1];
-        int port = atoi(argv[2]);
-        run_client(ip_str, port);
+        run_client(argv[1], atoi(argv[2]));
     } else {
         fprintf(stderr, "Usage:\n");
         fprintf(stderr, "  Server: %s <port>\n", argv[0]);
@@ -181,17 +196,13 @@ int main(int argc, char **argv) {
     }
 
     sender_pid = fork();
-    if (sender_pid == 0) {
-        audio_sender(socket_fd);
-    }
+    if (sender_pid == 0) audio_sender(socket_fd);
 
     receiver_pid = fork();
-    if (receiver_pid == 0) {
-        audio_receiver(socket_fd);
-    }
+    if (receiver_pid == 0) audio_receiver(socket_fd);
 
     wait(NULL);
-    wait(NULL); // 2つの子プロセスを待つ
+    wait(NULL);
     cleanup();
 
     return 0;
